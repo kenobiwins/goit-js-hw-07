5af07d5d6aa9535f99dcbaba83c987b8fa7a3bd9
Max Sidorenko <fivemoshr@gmail.com>
2022-10-07 19:53:32 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// items refs
+const galleryRef = document.querySelector("div.gallery");
+
+// add event listeners
+galleryRef.addEventListener("click", showFullImg);
+
+// functions
+function createElement(arr) {
+    return arr.reduce((acc, {original,preview,description}) => acc += `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" loading="lazy"/>
+    </a>
+    </div>`, '');
+}
+
+function insertElement(string) {
+    return galleryRef.insertAdjacentHTML("beforeend",string);
+}
+
+function showImg(e) {
+    const { target } = e;
+    const imgWindow = basicLightbox.create(`
+	<img src="${target.getAttribute("data-source")}" alt="${target.getAttribute("alt")}">
+	`);
+    imgWindow.show();
+// add and remuve event listener on esc
+    if (imgWindow.show()) {
+        window.addEventListener("keydown",closeOnEsc)
+    } else if (!imgWindow.show()) {
+        window.removeEventListener('keydown',closeOnEsc)
+    }
+
+    function closeOnEsc(e) {
+        if (e.code === "Escape") {
+            imgWindow.close()
+        }
+}
+};
+
+function showFullImg(e) {
+    e.preventDefault()
+    const { target } = e;
+    const image = target.nodeName == "IMG";
+
+    if (!image) {
+        return
+    } else if (image) {
+        showImg(e);
+    }
+};
+
+// call functions
+insertElement(createElement(galleryItems));
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function createElement(arr) {
+    return arr.reduce((acc, { original, preview, description }) => acc += `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" loading="lazy"/>
+    </a>`, '');
+}
+
+function insertElement(string) {
+    return galleryRef.insertAdjacentHTML("beforeend",string);
+}
+
+insertElement(createElement(galleryItems))
+
+
+var lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+
